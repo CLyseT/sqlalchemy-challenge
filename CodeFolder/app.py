c17b1e485c21db46fc4cba1944a4c94d753c7f67
CLyseT <83233764+CLyseT@users.noreply.github.com>
2021-08-08 10:39:21 -0400

    Updating Code app.py
    Updating code to reflect changes

--- CodeFolder/app.py
+++ CodeFolder/app.py
@@ -5,9 +5,19 @@ import sqlalchemy
 from sqlalchemy.ext.automap import automap_base
 from sqlalchemy.orm import Session
 from sqlalchemy import create_engine, func
+from sqlalchemy.ext.declarative import declarative_base
+Base = declarative_base()
 
 from flask import Flask, jsonify
-                    
+
+engine = create_engine("sqlite:///hawaii.sqlite")
+
+Base = automap_base()
+Base.prepare(engine, reflect=True)
+       
+session = Session(engine)
+
+
 #Flask Setup
 
 app = Flask(__name__)
@@ -32,17 +42,17 @@ def precipitation():
      Base = automap_base()
      Base.prepare(engine, reflect=True)
      
-     measurement = Base.classes.measurement
+     Measurement = Base.classes.measurement
      
      session = Session(engine)
-     data = session.query(measurement.date, measurement.prcp).all()
+     data = session.query(Measurement).all()
 
      all_precipitation = []
      
-     for data in data:
+     for precipitation in data:
          precipitation_dict = {}
-         precipitation_dict["date"] = data[0]
-         precipitation_dict["prcp"] = float(data[1])
+         precipitation_dict["date"] = precipitation.date
+         precipitation_dict["prcp"] = precipitation.prcp
          all_precipitation.append(precipitation_dict)
      
      session.close() 
@@ -57,10 +67,10 @@ def station():
      Base = automap_base()
      Base.prepare(engine, reflect=True)
 
-     measurement = Base.classes.measurement
+     Measurement = Base.classes.measurement
 
      session = Session(engine)
-     data_two = session.query(measurement.station).all()
+     data_two = session.query(Measurement.station).all()
 
      all_stations = list(np.ravel(data_two))
 
@@ -75,22 +85,22 @@ def tobs():
      Base = automap_base()
      Base.prepare(engine, reflect=True)
 
-     measurement = Base.classes.measurement
+     Measurement = Base.classes.measurement
      session = Session(engine)
 
      #Filter by the most active station for the last year
-     year_data = session.query(measurement.date, measurement.tobs).\
-         filter(measurement.station == 'USC00519281').\
-         filter(measurement.date >= '2016-08-23').all()
+     year_data = session.query(Measurement.date, Measurement.tobs).\
+         filter(Measurement.station == 'USC00519281').\
+         filter(Measurement.date >= '2016-08-23').all()
 
      #Return a JSON list of temperature observation for the previous year
      temp_list = []
 
-     for year_data in year_data:
+     for tobs in year_data:
          tobs_dict = {}
-         tobs_dict["date"] = year_data[0]
-         tobs_dict["tobs"] = float(year_data[1])
-         temp_list.append(temp_list)
+         tobs_dict["date"] = tobs.date
+         tobs_dict["tobs"] = tobs.tobs
+         temp_list.append(tobs_dict)
      
      session.close()
     
@@ -103,23 +113,23 @@ def start(start):
      Base = automap_base()
      Base.prepare(engine, reflect=True)  
 
-     measurement = Base.classes.measurement
+     Measurement = Base.classes.measurement
      session = Session(engine)
 
      #Set Start and End Dates
-     start_date = session.query(func.min(measurement.date)).first()[0]
-     end_date = session.query(func.max(measurement.date)).first()[0]
+     start_date = session.query(func.min(Measurement.date)).first()[0]
+     end_date = session.query(func.max(Measurement.date)).first()[0]
      
      #Set parameters
      if start >= start_date:
-        new_temp = session.query(func.min(measurement.tobs), func.avg(measurement.tobs), func.max(measurement.tobs)).\
-            filter(measurement.date >= start)
+        new_temp = session.query(func.min(Measurement.tobs), func.avg(Measurement.tobs), func.max(Measurement.tobs)).\
+            filter(Measurement.date >= start)
         
      if start <= end_date:
-        new_temp = session.query(func.min(measurement.tobs), func.avg(measurement.tobs), func.max(measurement.tobs)).\
-            filter(measurement.date <= end_date).all()[0]
+        new_temp = session.query(func.min(Measurement.tobs), func.avg(Measurement.tobs), func.max(Measurement.tobs)).\
+            filter(Measurement.date <= end_date).all()[0]
 
-        return jsonify (f"Minimum Temp: {new_temp[0]}<br/>"
+        return (f"Minimum Temp: {new_temp[0]}<br/>"
                         f"Average Temp: {new_temp[1]}<br/>"
                         f"Maximum Temp: {new_temp[2]}"
         )
@@ -133,23 +143,23 @@ def start_end(start, end):
      Base = automap_base()
      Base.prepare(engine, reflect=True)   
 
-     measurement = Base.classes.measurement
+     Measurement = Base.classes.measurement
      session = Session(engine)
 
      #Set Start and End Dates
-     start_date = session.query(func.min(measurement.date)).first()[0]
-     end_date = session.query(func.max(measurement.date)).first()[0]
+     start_date = session.query(func.min(Measurement.date)).first()[0]
+     end_date = session.query(func.max(Measurement.date)).first()[0]
      
      #Set parameters
      if start >= start_date:
-        new_temp = session.query(func.min(measurement.tobs), func.avg(measurement.tobs), func.max(measurement.tobs)).\
-            filter(measurement.date >= start)
+        new_temp = session.query(func.min(Measurement.tobs), func.avg(Measurement.tobs), func.max(Measurement.tobs)).\
+            filter(Measurement.date >= start)
 
      if end <= end_date:
-        new_temp = session.query(func.min(measurement.tobs), func.avg(measurement.tobs), func.max(measurement.tobs)).\
-        filter(measurement.date <= end)
+        new_temp = session.query(func.min(Measurement.tobs), func.avg(Measurement.tobs), func.max(Measurement.tobs)).\
+        filter(Measurement.date <= end)
         
-        return jsonify (f"Minimum Temp: {new_temp[0]}<br/>"
+        return (f"Minimum Temp: {new_temp[0]}<br/>"
                         f"Average Temp: {new_temp[1]}<br/>"
                         f"Maximum Temp: {new_temp[2]}"
         )
